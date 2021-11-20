10c475f441b5220db606ce8ea452b2b6bf180960
aoirint <aoirint@gmail.com>
2021-11-20 23:42:10 +0900

    use effect

--- src/components/useGitHubRecentRepos.ts
+++ src/components/useGitHubRecentRepos.ts
@@ -12,32 +12,34 @@ const useGitHubRecentRepos = () => {
     const [loading, setLoading] = React.useState<boolean>(true)
     const [repos, setRepos] = React.useState<Repo[]>(null)
 
-    if (repos === null) {
+    React.useEffect(() => {
+      if (repos === null) {
         fetch('https://api.github.com/users/aoirint/repos?sort=updated&per_page=10')
-            .then((data) => data.json())
-            .then((data) => {
-                const repos = data.map((repo): Repo => {
-                    const title = repo.full_name
-                    const url = repo.html_url
+        .then((data) => data.json())
+        .then((data) => {
+          const repos = data.map((repo): Repo => {
+            const title = repo.full_name
+            const url = repo.html_url
 
-                    const createdAt = dayjs(repo.created_at).format('YYYY-MM-DD')
-                    const updatedAt = dayjs(repo.updated_at).format('YYYY-MM-DD')
+            const createdAt = dayjs(repo.created_at).format('YYYY-MM-DD')
+            const updatedAt = dayjs(repo.updated_at).format('YYYY-MM-DD')
 
-                    return {
-                        title,
-                        url,
-                        createdAt,
-                        updatedAt,
-                    }
-                })
+            return {
+              title,
+              url,
+              createdAt,
+              updatedAt,
+            }
+          })
 
-                setRepos(repos)
-                setLoading(false)
-            })
-            .catch((error: unknown) => {
-                console.error(error)
-            })
-    }
+          setRepos(repos)
+          setLoading(false)
+        })
+        .catch((error: unknown) => {
+          console.error(error)
+        })
+      }
+    })
 
     return {
         loading,
