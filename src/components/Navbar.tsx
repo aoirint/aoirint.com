6dc5511ddea98a1903a2e7f86b104ea8a630cd49
aoirint <aoirint@gmail.com>
2021-11-21 00:01:58 +0900

    nav toggle working

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import icon from '../images/icon.png'
 
 const Navbar: React.FC<{}> = () => {
+    const [active, setActive] = React.useState<boolean>(false)
+
     return (
         <nav className="navbar" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
@@ -9,14 +11,16 @@ const Navbar: React.FC<{}> = () => {
                   <img src={icon} alt="Logo image" width="28" height="28" />
                 </a>
 
-                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <a role="button" className={`navbar-burger ${active ? 'is-active' : ''}`} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" onClick={() => {
+                  setActive(!active)
+                }}>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                 </a>
             </div>
 
-            <div id="navbarBasicExample" className="navbar-menu">
+            <div id="navbarBasicExample" className={`navbar-menu ${active ? 'is-active' : ''}`}>
                 <div className="navbar-start">
                     <a className="navbar-item" href="/">
                         Home
