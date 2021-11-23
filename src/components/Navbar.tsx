3bf08c570daf94db6dd76c0711d3a171b3fbdeb5
aoirint <aoirint@gmail.com>
2021-11-23 23:03:28 +0900

    fix link titles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -48,17 +48,20 @@ const Navbar: React.FC<{}> = () => {
               <a className="navbar-item" href="https://qiita.com/aoirint" target='_blank'>
                 Qiita
               </a>
-              <a className="navbar-item" href="https://aoirint.hatenablog.com/" target='_blank'>
-                はてなブログ
-              </a>
               <a className="navbar-item" href="https://blog.aoirint.com/" target='_blank'>
-                技術ブログ
+                技術ノート
+              </a>
+              <a className="navbar-item" href="https://aoirint.hatenablog.com/" target='_blank'>
+                技術ノート（旧）
               </a>
               <a className="navbar-item" href="https://www.resume.id/aoirint" target='_blank'>
                 resume.id
               </a>
               <a className="navbar-item" href="https://diary.aoirint.com/" target='_blank'>
                 Diary
+              </a>
+              <a className="navbar-item" href="https://aoirint.hatenadiary.jp/" target='_blank'>
+                Diary（旧）
               </a>
             </div>
           </div>
